b0790150d8102e7269fb0897f46f324e3a90a3f4
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-25 20:01:06 +0200

    Setup ducks patterns.

---
+++ src/ducks/physicianSearch/index.ts
@@ -0,0 +1,5 @@
+// import physicianSearchReducer from "./reducers";
+export { default as physicianSearch } from "./physicianSearchComponent";
+export { default as physicianSearchActions } from "./actions";
+export { default as physicianSearchOperations } from "./operations";
+// export default physicianSearchReducer;
