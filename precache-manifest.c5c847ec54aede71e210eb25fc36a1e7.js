c09feed6dbf49b21bd62fe04097681d7f0df1b5e
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-25 15:01:45 +0200

    Updates

---
+++ precache-manifest.c5c847ec54aede71e210eb25fc36a1e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e91b76ff8b955b6bf158e5cb1ed7b13",
+    "url": "/artzsuche/index.html"
+  },
+  {
+    "revision": "abe0b456f31ab292bd5c",
+    "url": "/artzsuche/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22d33c8636de2b47ef40",
+    "url": "/artzsuche/static/js/2.f5247132.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/artzsuche/static/js/2.f5247132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe0b456f31ab292bd5c",
+    "url": "/artzsuche/static/js/main.60485c52.chunk.js"
+  },
+  {
+    "revision": "43dc10f30bab9aad9947",
+    "url": "/artzsuche/static/js/runtime-main.8bcdaec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/artzsuche/static/media/logo.5d5d9eef.svg"
+  }
+]);
