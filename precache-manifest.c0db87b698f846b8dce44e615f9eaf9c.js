277b6396b3c539105aa5f31c43506b3de04e5184
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-25 21:36:21 +0200

    Updates

--- precache-manifest.c0db87b698f846b8dce44e615f9eaf9c.js
+++ precache-manifest.c0db87b698f846b8dce44e615f9eaf9c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9fe8ad0780cf5b319f7e626e51f99f7d",
+    "revision": "2cc0c5813519a130196dd861f0ac600d",
     "url": "/artzsuche/index.html"
   },
   {
-    "revision": "24bf747c3d52afa0ddfb",
+    "revision": "87ee6b4ac4c2b6e6d2e6",
     "url": "/artzsuche/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/artzsuche/static/js/2.07d82875.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "24bf747c3d52afa0ddfb",
-    "url": "/artzsuche/static/js/main.58c0e4aa.chunk.js"
+    "revision": "87ee6b4ac4c2b6e6d2e6",
+    "url": "/artzsuche/static/js/main.0cdf4cec.chunk.js"
   },
   {
     "revision": "43dc10f30bab9aad9947",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/artzsuche/precache-manifest.5f02367a1eea0cf20783f842a69f22af.js"
+  "/artzsuche/precache-manifest.c0db87b698f846b8dce44e615f9eaf9c.js"
 );
 
 self.addEventListener('message', (event) => {
