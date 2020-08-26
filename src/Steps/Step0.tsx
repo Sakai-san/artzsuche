653629ee774edb2fbefd708b278bf56a6572aab0
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-27 01:02:29 +0200

    Cantons done.

--- src/Steps/Step0.tsx
+++ src/Steps/Step0.tsx
@@ -2,6 +2,9 @@ import React, { FunctionComponent } from "react";
 import { IStepProps } from "./StepType";
 import Paper from "@material-ui/core/Paper";
 import DomainRoundedIcon from "@material-ui/icons/DomainRounded";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import cantons from "../cantons.json";
 
 const Step0: FunctionComponent<IStepProps> = ({
   response,
@@ -9,8 +12,8 @@ const Step0: FunctionComponent<IStepProps> = ({
   setCurrentStep,
   className,
 }) => {
-  const onChangeHandler = (e: any) => {
-    setResponse(e.target.value);
+  const onChangeHandler = (e: any, value: any) => {
+    setResponse(value);
     setCurrentStep(1);
   };
 
@@ -21,11 +24,20 @@ const Step0: FunctionComponent<IStepProps> = ({
           <DomainRoundedIcon fontSize="large" /> Im welchem Kanton wohnst du ?
         </span>
         {!response && (
-          <select onChange={onChangeHandler}>
-            <option value="zug">Zug</option>
-            <option value="zuerich">Zuerich</option>
-            <option value="stgallen">St. Gallen</option>
-          </select>
+          <Autocomplete
+            id="combo-box-physicians"
+            options={cantons}
+            getOptionLabel={(option: string) => option}
+            style={{ width: 300 }}
+            onChange={onChangeHandler}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Suche nach deinem Artz"
+                variant="outlined"
+              />
+            )}
+          />
         )}
       </div>
 
