992a4085f78459610e3a7421ce6c9cdfba293249
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-27 08:26:56 +0200

    Resource url.

--- src/ducks/physicians/operations.ts
+++ src/ducks/physicians/operations.ts
@@ -2,7 +2,7 @@ import { Dispatch } from "redux";
 import actions from "./actions";
 
 const fetchPhysicians = (dispatch: Dispatch) => {
-  fetch("https://sakai-san.github.io/sharedatadifferentroutes/books.json")
+  fetch("https://sakai-san.github.io/artzsuche/physicians.json")
     .then((r) => r.json())
     .then((r) => dispatch(actions.makePhysiciansFetch(r || [])));
 };
