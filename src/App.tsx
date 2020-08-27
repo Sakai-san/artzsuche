ccc08444e6c4ce9face13f68c3d5832f97853f89
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-27 07:32:05 +0200

    Toolbar.

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,51 @@ import React, { FunctionComponent, useState } from "react";
 import Step0 from "./Steps/Step0";
 import Step1 from "./Steps/Step1";
 import Step2 from "./Steps/Step2";
-import { createStyles, Theme, makeStyles } from "@material-ui/core";
+import { Theme, makeStyles } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import { deepOrange } from "@material-ui/core/colors";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      padding: "20px",
-      "&>section:not(:first-child)": {
-        padding: "5px",
-        marginTop: "30px",
-        borderRadius: "6px 6px",
-      },
+const useStyles = makeStyles((theme: Theme) => ({
+  toolbar: {
+    backgroundColor: "#FF51A1",
+    minHeight: 80,
+  },
+  root: {
+    padding: "20px",
+    "&>section:not(:first-child)": {
+      padding: "5px",
+      marginTop: "30px",
+      borderRadius: "6px 6px",
     },
-    lanes: {
-      display: "flex",
-      "&>div:last-child": {
-        marginLeft: "auto",
-      },
+  },
+  lanes: {
+    display: "flex",
+    "&>div:last-child": {
+      marginLeft: "auto",
     },
-    orange: {
-      color: theme.palette.getContrastText(deepOrange[500]),
-      backgroundColor: deepOrange[500],
+  },
+  orange: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+  },
+  bot: {
+    fontSize: "35px",
+  },
+  step: {
+    display: "flex",
+    flexDirection: "column",
+    "&>div:nth-child(2)": {
+      marginLeft: "auto",
     },
-    bot: {
-      fontSize: "35px",
+    "& svg": {
+      position: "relative",
+      top: "6px",
     },
-    step: {
-      display: "flex",
-      flexDirection: "column",
-      "&>div:nth-child(2)": {
-        marginLeft: "auto",
-      },
-      "& svg": {
-        position: "relative",
-        top: "6px",
-      },
-    },
-  })
-);
+  },
+}));
 
 const App: FunctionComponent = () => {
   const classes = useStyles();
@@ -51,42 +56,52 @@ const App: FunctionComponent = () => {
   const [step2Response, setStep2Response] = useState<string | null>(null);
 
   return (
-    <div className={classes.root}>
-      <section className={classes.lanes}>
-        <div>
-          <Avatar alt="bot" className={classes.bot}>
-            <span>&#129302;</span>
-          </Avatar>
-        </div>
-        <div>
-          <Avatar alt="you" className={classes.orange}>
-            Du
-          </Avatar>
-        </div>
-      </section>
-      {[
-        <Step0
-          className={classes.step}
-          key="step0"
-          response={step0Response}
-          setResponse={setStep0Response}
-          setCurrentStep={setCurrentStep}
-        />,
-        <Step1
-          className={classes.step}
-          key="step1"
-          response={step1Response}
-          setResponse={setStep1Response}
-          setCurrentStep={setCurrentStep}
-        />,
-        <Step2
-          className={classes.step}
-          key="step2"
-          response={step2Response}
-          setResponse={setStep2Response}
-          setCurrentStep={setCurrentStep}
-        />,
-      ].slice(0, currentStep + 1)}
+    <div>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6" color="inherit">
+            Lass uns dikutieren
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <div className={classes.root}>
+        <section className={classes.lanes}>
+          <div>
+            <Avatar alt="bot" className={classes.bot}>
+              <span>&#129302;</span>
+            </Avatar>
+          </div>
+          <div>
+            <Avatar alt="you" className={classes.orange}>
+              Du
+            </Avatar>
+          </div>
+        </section>
+        {[
+          <Step0
+            className={classes.step}
+            key="step0"
+            response={step0Response}
+            setResponse={setStep0Response}
+            setCurrentStep={setCurrentStep}
+          />,
+          <Step1
+            className={classes.step}
+            key="step1"
+            response={step1Response}
+            setResponse={setStep1Response}
+            setCurrentStep={setCurrentStep}
+          />,
+          <Step2
+            className={classes.step}
+            key="step2"
+            response={step2Response}
+            setResponse={setStep2Response}
+            setCurrentStep={setCurrentStep}
+          />,
+        ].slice(0, currentStep + 1)}
+      </div>
     </div>
   );
 };
