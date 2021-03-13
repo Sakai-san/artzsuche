8a3f345b5a92cce964a36852bc1c9e3d38348c0a
Sakai-san <o_edo@hotmail.com>
2021-03-13 17:43:59 +0100

    Simplify code.

--- src/ReactBotForm/Write.tsx
+++ src/ReactBotForm/Write.tsx
@@ -1,52 +1,29 @@
-import React, { FunctionComponent, useContext } from "react";
-import { Theme, makeStyles } from "@material-ui/core";
+import { FunctionComponent, useContext } from "react";
 import useFocus from "./useFocus";
 import { ReactBotFormChildContext, ReactBotFormContext } from "./Context";
 import { WriteProps } from "./types";
-
-const useStyles = makeStyles((theme: Theme) => ({
-  inputElementVisible: {
-    visibility: "visible",
-    opacity: 1,
-    transition: "opacity 0.2s ease-in",
-  },
-  inputElementHidden: {
-    visibility: "hidden",
-    opacity: 0,
-  },
-}));
 
 const Write: FunctionComponent<WriteProps> = ({ children, doValidation }) => {
   const { input, setResponse, index, setIsValid } = useContext(
     ReactBotFormChildContext
   );
-  const { responseInEdition, setResponseInEdition, isBotTyping } = useContext(
+  const { responseInEdition, setResponseInEdition } = useContext(
     ReactBotFormContext
   );
 
-  const classes = useStyles();
   const domRef = useFocus(responseInEdition, index);
 
-  return (
-    <div
-      className={
-        isBotTyping ? classes.inputElementHidden : classes.inputElementVisible
-      }
-    >
-      {children?.({
-        index,
-        doValidation,
-        input,
-        setResponse,
-        responseInEdition,
-        setResponseInEdition,
-        setIsValid,
-        domRef,
-      })}
-    </div>
-  );
+  return children?.({
+    index,
+    doValidation,
+    input,
+    setResponse,
+    responseInEdition,
+    setResponseInEdition,
+    setIsValid,
+    domRef,
+  });
 };
-
 export default Write;
 
 /*
