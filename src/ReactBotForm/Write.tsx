30e8b817ed4bf50ca3cc0da2156bfddcfad64e7e
Sakai-san <o_edo@hotmail.com>
2021-03-15 10:23:08 +0100

    Transition.

--- src/ReactBotForm/Write.tsx
+++ src/ReactBotForm/Write.tsx
@@ -1,7 +1,26 @@
-import { FunctionComponent, useContext } from "react";
+import React, { FunctionComponent, useContext } from "react";
+import { makeStyles, createStyles } from "@material-ui/core";
 import useFocus from "./useFocus";
 import { ReactBotFormChildContext, ReactBotFormContext } from "./Context";
 import { WriteProps } from "./types";
+
+const ANIMATION_DURATION = 500;
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    "@keyframes display": {
+      from: {
+        opacity: 0,
+      },
+      to: {
+        opacity: 1,
+      },
+    },
+    display: {
+      animation: `$display ${ANIMATION_DURATION}ms ease-in`,
+    },
+  })
+);
 
 const Write: FunctionComponent<WriteProps> = ({ children, doValidation }) => {
   const { input, setResponse, index, setIsValid } = useContext(
@@ -10,29 +29,23 @@ const Write: FunctionComponent<WriteProps> = ({ children, doValidation }) => {
   const { responseInEdition, setResponseInEdition } = useContext(
     ReactBotFormContext
   );
-
+  const classes = useStyles();
   const domRef = useFocus(responseInEdition, index);
-  return children?.({
-    index,
-    doValidation,
-    input,
-    setResponse,
-    responseInEdition,
-    setResponseInEdition,
-    setIsValid,
-    domRef,
-  });
+
+  return (
+    <div className={classes.display}>
+      {children?.({
+        index,
+        doValidation,
+        input,
+        setResponse,
+        responseInEdition,
+        setResponseInEdition,
+        setIsValid,
+        domRef,
+      })}
+    </div>
+  );
 };
 
 export default Write;
-
-/*
-CloseIcon from \"@material-ui/icons/Close\"\nimport { makeStyles, createStyles } from \"@material-ui/core\"\n\nimport { monaco } from \"react-monaco-editor\"\n\nconst ANIMATION_DURATION = 3000\n\nconst useStyles = makeStyles((theme) =>\n\tcreateStyles({\n\t\t\"@keyframes lineshighlight\": {\n\t\t\tfrom: {\n\t\t\t\topacity: 1,\n\t\t\t},\n\t\t\tto: {\n\t\t\t\topacity: 0,\n\t\t\t},\n\t\t},\n\t\thighligh
-tLines: {\n\t\t\tbackgroundColor: (props: definition.BaseProps) =>\n\t\t\t\tstyles.getColor({ intention: \"info\" }, theme, props.context) ||\n\t\t\t\t\"pink\",\n\t\t\tanimation: `$lineshighlight ${ANIMATION_DURATION}ms ease-in-out`,\n\t\t},\n\t})\n)\n\nconst CodeToolbar: FunctionComponent<definition.BaseProps> = (props) => {\n\tconst classes = useStyles(props)\n\tconst uesio = hooks.useUesio(props)\n\tconst yamlDoc = ue
-sio.view.useYAML()\n\tconst currentYaml = yamlDoc?.toString() || \"\"\n\tconst lastModifiedNode = uesio.builder.useLastModifiedNode()\n\n\tconst currentAST = useRef<yaml.Document | undefined>(yamlDoc)\n\tcurrentA
-ST.current = util.yaml.parse(currentYaml)\n\n\tconst editorRef = useRef<monaco.editor.IStandaloneCodeEditor | undefined>(\n\t\tundefined\n\t)\n\tconst monacoRef = useRef<typeof monaco | undefined>(undefined)\n\tc
-onst decorationsRef = useRef<string[] | undefined>(undefined)\n\n\tconst e = editorRef.current\n\tconst m = monacoRef.current\n\n\tuseEffect(() => {\n\t\tif (e && m && currentAST.current && lastModifiedNode) {\n\
-t\t\tconst node = util.yaml.getNodeAtPath(\n\t\t\t\tlastModifiedNode,\n\t\t\t\tcurrentAST.current.contents\n\t\t\t)\n\t\t\tconst model = e.getModel()\n\t\t\tif (!node || !model) return\n\t\t\tconst range = node.r
-ange\n\t\t\tif (!range || !range.length) return\n\n\t\t\tc
-
-*/
